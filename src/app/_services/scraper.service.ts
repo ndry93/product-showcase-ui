b42a5f45356c2d893de1501775f51dd28c10ede5
Hendry Yuwono <ndry93@yahoo.co.id>
2020-05-31 23:29:37 +0700

    add missing content-type header

--- src/app/_services/scraper.service.ts
+++ src/app/_services/scraper.service.ts
@@ -1,22 +1,23 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { environment } from "environments/environment";
 import { ScraperDTO } from "@/_models";
 
 @Injectable({ providedIn: "root" })
 export class ScraperService {
+  headers = new  HttpHeaders().set("Content-Type", "application/json");
   constructor(private http: HttpClient) {}
 
   getAll() {
-    return this.http.get(`${environment.apiUrl}/scraper/all`);
+    return this.http.get(`${environment.apiUrl}/scraper/all`, { headers: this.headers });
   }
 
   getById(id: string) {
-    return this.http.get(`${environment.apiUrl}/scraper/${id}`);
+    return this.http.get(`${environment.apiUrl}/scraper/${id}`, { headers: this.headers });
   }
 
   register(scraperDto: ScraperDTO) {
-    return this.http.post(`${environment.apiUrl}/scraper`, scraperDto);
+    return this.http.post(`${environment.apiUrl}/scraper`, scraperDto, { headers: this.headers });
   }
 
   // update(scraperDto: ScraperDTO) {
@@ -24,6 +25,6 @@ export class ScraperService {
   // }
 
   delete(id: number) {
-    return this.http.delete(`${environment.apiUrl}/scraper/${id}`);
+    return this.http.delete(`${environment.apiUrl}/scraper/${id}`, { headers: this.headers });
   }
 }
